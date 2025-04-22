d7eb05519eff31244ff907bedca6b8284b85dafa
juandavidbaro <juandavidbaro@gmail.com>
2025-04-21 19:34:24 -0500

    Add project

---
+++ bank-account/account.query/src/main/java/com/techbank/account/query/infrastructure/consumers/EventConsumer.java
@@ -0,0 +1,14 @@
+package com.techbank.account.query.infrastructure.consumers;
+
+import com.techbank.account.common.events.AccountClosedEvent;
+import com.techbank.account.common.events.AccountOpenedEvent;
+import com.techbank.account.common.events.FundsDepositedEvent;
+import com.techbank.account.common.events.FundsWithdrawnEvent;
+import com.techbank.cqrs.core.events.BaseEvent;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.messaging.handler.annotation.Payload;
+
+public interface EventConsumer {
+    void consume(@Payload BaseEvent event, Acknowledgment ack);
+
+}
